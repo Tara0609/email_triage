6b28fcdb22d92d1ca3c2c2a4f78756f81030921f
Taradrita Routh <taradrita.routh0609@gmail.com>
2026-04-02 00:53:28 +0530

    initial

--- app.py
+++ app.py
@@ -107,4 +107,4 @@ def state():
 # ---------------------------------------------------------------------------
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=7860, reload=False)
+    uvicorn.run("app:app", host="127.0.0.1", port=7861, reload=False)
